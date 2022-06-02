6d0cdca35e1404cf8a0e20093d0fad33b807f33f
KAEM10 <101412093+KAEM10@users.noreply.github.com>
2022-06-02 10:48:55 -0500

    02/06/2022 - agregar lista
    agrega, todo lo relacionado con lista, se me habia olvidado

---
+++ libColecciones-Teoria/libColecciones-Teoria/Enlazadas/clsListaDobleEnlazada.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Servicios.Colecciones.Interfaces;
+
+namespace Servicios.Colecciones.Enlazadas
+{
+    public class clsListaDobleEnlazada<Tipo> : iLista<Tipo> where Tipo : IComparable<Tipo>
+    {
+        #region Atributos
+        #region Asociativos
+        private Servicios.Colecciones.Nodos.clsNodoDobleEnlazado<Tipo> atrPrimero;
+        private Servicios.Colecciones.Nodos.clsNodoDobleEnlazado<Tipo> atrUltimo;
+        #endregion
+        #endregion
+        #region Metodos
+        #region CRUD
+        public bool agregar(Tipo prmItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool insertar(int prmIndice, Tipo prmItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool remover(int prmIndice, ref Tipo prmItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool modificar(int prmindice, Tipo prmItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+        #region QUERY
+        public bool encontrar(Tipo prmItem, ref int prmIndice)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+        #endregion
+    }
+}
---
+++ libColecciones-Teoria/libColecciones-Teoria/Interfaces/iLista.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicios.Colecciones.Interfaces
+{
+    public interface iLista<Tipo> where Tipo : IComparable<Tipo>
+    {
+        #region Metodos
+        #region CRUD
+        bool agregar(Tipo prmItem);
+
+        bool insertar(int prmIndice, Tipo prmItem);
+
+        bool remover(int prmIndice, ref Tipo prmItem);
+
+        bool modificar(int prmindice, Tipo prmItem);
+
+        #endregion
+        #region QUERY
+        bool encontrar(Tipo prmItem, ref int prmIndice);
+        #endregion
+        #endregion
+    }
+}
