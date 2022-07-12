dad4be2e2e42eb3f50c2201e5fa80e3ad185fca5
CamiloR293 <jrodallega@unicauca.edu.co>
2022-07-12 09:57:51 -0500

    Borrar CRUD mal hechos

--- libColecciones-Teoria/libColecciones-Teoria/Enlazadas/clsPilaEnlazada.cs
+++ libColecciones-Teoria/libColecciones-Teoria/Enlazadas/clsPilaEnlazada.cs
@@ -68,94 +68,26 @@ namespace Servicios.Colecciones.Enlazadas
         public bool apilar(Tipo prmItem)
         {
             bool apilo = false;
-            if (atrCapacidad == 0)
-            {
-                atrCapacidad = atrFactorCrecimiento;
-                atrItems = new Tipo[atrCapacidad];
-            }
-            if ((atrCapacidad != int.MaxValue / 16) && (atrCapacidad == atrLongitud) && atrDinamica)
-            {
-                Tipo[] atrItemsAux = new Tipo[atrFactorCrecimiento + atrCapacidad];
-                Array.Copy(atrItems, atrItemsAux, atrItems.Length);
-                atrItems = atrItemsAux;
-
-                atrCapacidad = atrFactorCrecimiento + atrCapacidad;
-            }
-            if (atrLongitud < atrCapacidad)
-            {
-                for (int i = atrLongitud + 1; i > 0; i--)
-                {
-                    atrItems[i] = atrItems[i - 1];
-                }
-                atrItems[0] = prmItem;
-                atrLongitud++;
-                apilo = true;
-            }
+            
             return apilo;
         }
         public bool desapilar(ref Tipo prmItem)
         {
             bool desapilo = false;
-            if (atrLongitud > 0)
-            {
-                prmItem = atrItems[0];
-                for (int i = 0; i < (atrLongitud - 1); i++)
-                {
-                    atrItems[i] = atrItems[i + 1];
-                }
-                desapilo = true;
-                atrLongitud--;
-            }
-
+            
             return desapilo;
         }
         public bool revisar(ref Tipo prmItem)
         {
             bool reviso = false;
-            if (atrLongitud > 0)
-            {
-                prmItem = atrItems[0];
-                reviso = true;
-            }
-            if (atrLongitud < 0)
-            {
-                atrItems = null;
-                prmItem = atrItems[0];
-                reviso = false;
-            }
+            
             return reviso;
         }
         public bool reversar()
         {
-            if (atrLongitud > 0)
-            {
-                Tipo aux;
-                int j = 0;
-                int end;
-                if (atrLongitud % 2 == 0)
-                {
-                    end = (atrLongitud) / 2;
-                }
-                else
-                {
-                    end = (atrLongitud - 1) / 2;
-                }
-                for (int i = atrLongitud - 1; i >= end; i--)
-                {
+            bool reverso = false;
 
-                    aux = atrItems[j];
-                    atrItems[j] = atrItems[i];
-                    atrItems[i] = aux;
-                    j++;
-                }
-                atrReversar = true;
-
-                return atrReversar;
-            }
-            else
-            {
-                return atrReversar;
-            }
+            return reverso;
         }
         #endregion
         #region Mutadores
